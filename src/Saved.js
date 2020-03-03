3498dc3ae7d0346b7574b420e639aeffc3a27768
James <james.re@yahoo.com>
2020-03-03 13:55:07 -0800

    umm

--- src/Saved.js
+++ src/Saved.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
 import request from 'superagent'
-import { getSaved, addSaved, deleteSaved } from './api.js'
+import {
+    getSaved,
+    addSaved,
+    deleteSaved
+} from './api.js'
 
 export default class Saved extends Component {
     state = {
         saved: []
     }
     async componentDidMount() {
-        
-      
+
+
 
         const data = await getSaved();
-        
-        this.setState({ saved: data.body });
-            
+
+        this.setState({
+            saved: data.body
+        });
+
     }
     handleFavorite = async () => {
-        this.setState({saved: this.state.saved})
+        this.setState({
+            saved: this.state.saved
+        })
     }
     render() {
-        return (
-            <div>
-                {this.state.saved.map(concert=> <SavedItem concert={concert} key={concert.id} />)}
-            </div>
-        )
-    }
-}
+            return ( <
+                div > {
+                    this.state.saved.map(concert => < SavedItem concert = {
+                            concert
+                        }
+                        key = {
+                            concert.id
+                        }
+                        />)} < /
+                        div >
+                    )
+                }
+            }
