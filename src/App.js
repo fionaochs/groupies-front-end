fcd8818996fb5671fdb4e5d4c3bad69dbd7f87a7
Owen Prendergast <prendergasto@gmail.com>
2020-03-03 14:19:39 -0800

    committing before pulling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-
-import React, { Component } from 'react';
-import { 
-  Route, 
-  BrowserRouter as Router, 
-} from 'react-router-dom';
-// import Header from './Header.js';
-// import Footer from './Footer.js';
-import HomePage from './HomePage.js';
-import './HomePage.css';
-
-export default class App extends Component {
-  render (){
-  return (
-    <Router>
-      <div>
-      {/* <Header /> */}
-      <Route exact path="/" component={HomePage} />
-      {/* <Footer /> */}
-      </div>
-    </Router>
-  )
-  }
-}
-
-
 import React from 'react';
 import './App.css';
 import {
@@ -36,6 +10,7 @@ import ConcertList from "./ConcertList.js"
 // import Header from './Header';
 import Detail from './Detail.js';
 // import AboutMe from './AboutUs.js';
+// import Footer from './Footer';
 
 
 export default class App extends React.Component {
