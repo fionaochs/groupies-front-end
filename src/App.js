5d6f76902d614728c9ec408961bfc52947e8209f
Hannah Riley <riley.hannahm@gmail.com>
2020-03-04 12:06:08 -0800

    some css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './SignIn.css'
 import {
   Route,
   Switch,
@@ -7,23 +8,25 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 import ConcertList from "./ConcertList.js"
-// import Header from './Header';
+import Header from './Header';
 import Detail from './Detail.js';
 import SignIn from './SignIn.js'
 import SignUp from './SignUp.js'
 import Saved from './Saved.js'
 import AboutUs from './AboutUs.js'
+import HomePage from './HomePage.js'
+
 export default class App extends React.Component {
   render() {
     return (
       <Router>
         <div>
-          {/* <Header /> */}
+          <Header />
           <Link to="/saved">Saved</Link>&nbsp;
           <Link to="/">go home</Link>
           <Link to="/about">about us</Link>
           <Switch>
-            <Route exact path="/" component={SignIn} />
+            <Route exact path="/" component={HomePage} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/saved" component={Saved} />
             <Route exact path="/concerts" component={ConcertList} />
