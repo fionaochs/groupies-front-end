3c91396599ac536b123ba95a4a40cfdd1f349b09
Fiona Ochs <fiona.oaks@gmail.com>
2020-03-04 08:43:29 -0800

    fiona

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import {
 import ConcertList from "./ConcertList.js"
 // import Header from './Header';
 import Detail from './Detail.js';
-// import AboutMe from './AboutUs.js';
 import SignIn from './SignIn.js'
 import SignUp from './SignUp.js'
+import AboutUs from './AboutUs.js'
+
+import './AboutUs.css'
 
 export default class App extends React.Component {
   render() {
@@ -20,13 +22,13 @@ export default class App extends React.Component {
         <div>
           {/* <Header /> */}
           <Link to="/">go home</Link>
-          <Link to="/about-me/about">about us</Link>
+          <Link to="/about">about us</Link>
           <Switch>
             <Route exact path="/" component={SignIn} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/concerts" component={ConcertList} />
             <Route exact path="/concert/:id" component={Detail} />
-            {/* <Route exact path="/:about-me/about" component={AboutMe} /> */}
+            <Route exact path="/about" component={AboutUs} />
           </Switch>
         </div>
       </Router>
