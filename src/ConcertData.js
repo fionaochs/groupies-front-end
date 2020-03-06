1166dbd0173dd9caffb7640d92f9e8682365c4ac
Logan Scott <logantscott@gmail.com>
2020-03-05 17:43:09 -0800

    styling n shit

--- src/ConcertData.js
+++ src/ConcertData.js
@@ -12,6 +12,7 @@ export default class ConcertData extends Component {
             <li className="concert-list">
                 <Link  to={`concert/${this.props.concert.id}`} key={this.props.concert.id}>
                 <img src={this.props.concert.images[0].url} style={{width: "125px", height:"100px"}} alt="" className="concert-images"/>
+                <div className='list-right'>
                 <h2>{this.props.concert.name}</h2>
                 {/* <h3>Genre: {this.props.concert.classifications[0].genre.name}</h3> */}
                 <h3>{concertdate}</h3>
@@ -21,6 +22,7 @@ export default class ConcertData extends Component {
                 {/* <h3>Maximum Price: {this.props.concert.priceRanges[0].max}</h3> */}
                 <h3>{this.props.concert._embedded.venues[0].name}</h3>
                 <h3>{this.props.concert._embedded.venues[0].city.name}, {this.props.concert._embedded.venues[0].state.name}</h3>
+                </div>
                 </Link>
                 <span className={this.props.saved.findIndex(el => el.tm_id === this.props.concert.id) !== -1 ? 'saved' : ''} 
                     onClick={ (e) => {this.props.handleSaved(this.props.concert, this.props.saved.findIndex(el => el.tm_id === this.props.concert.id), e)}}>♫<div></div><div></div><div></div><div></div></span>
