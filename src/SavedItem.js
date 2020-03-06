a000dad1762a8ff2a1d1661aa32ea418b7ac5c34
Hannah Riley <riley.hannahm@gmail.com>
2020-03-06 11:05:32 -0800

    trying again

--- src/SavedItem.js
+++ src/SavedItem.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import './Saved.css'
 import {Link} from 'react-router-dom'
+import request from 'superagent'
 // import * as moment from 'moment';
 
 export default class SavedItem extends Component {
+
     render() {
         
         return (
@@ -13,12 +15,11 @@ export default class SavedItem extends Component {
                 <div className="saved-list">
                 <h1>{this.props.concert.name}</h1>
                 <img src={this.props.concert.images} style={{width: "125px", height:"100px"}} alt="" className="concert-images"/>
-
-
                 <h3>{this.props.concert.city}, {this.props.concert.state}</h3>
-
                 </div>
                 </a>
+                <button id="remove-from-saves-button" onClick={() => this.props.handleDelete(this.props.concert)}>Remove from saved</button>
+                
             </div>
         )
     }
